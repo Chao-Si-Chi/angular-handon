244862becbc4034f72065b9b243dfed7734d8aa6
Chao-Si-Chi <128582972+Chao-Si-Chi@users.noreply.github.com>
2023-03-30 17:12:03 +0800

    2 files modified

--- src/app/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-edit/shopping-edit.component.ts
@@ -10,6 +10,7 @@ export class ShoppingEditComponent implements OnInit {
   //取得樣板變數HTML:#nameInput
   @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
   @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  index;
   constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit() {
@@ -19,5 +20,20 @@ export class ShoppingEditComponent implements OnInit {
     const ingredient = {name: this.nameInput.nativeElement.value,
                         amount: parseInt(this.amountInput.nativeElement.value)};
     this.shoppingService.addIngredient(ingredient);
+  }
+
+  onDelete(){
+    //取得頁面輸入的資料
+    const ingredient = {name: this.nameInput.nativeElement.value,
+                        amount: parseInt(this.amountInput.nativeElement.value)};
+    //取得ARRAY的INDEX值
+    this.index = this.shoppingService.ingredients.findIndex(x => x.name === ingredient.name && x.amount === ingredient.amount)
+    //console.log("index: " + this.index);
+    this.shoppingService.removeIngredient(this.index);
+  }
+
+  onClear(){
+    this.nameInput.nativeElement.value = '';
+    this.amountInput.nativeElement.value = '';
   }
 }
